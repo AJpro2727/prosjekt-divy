503553c5430d164f9324a5dd30fd0b83ef894a6f
AJpro2727 <aleksander.joergensen@gmail.com>
2026-03-06 11:35:28 +0100

    v3

--- logic.js
+++ logic.js
@@ -6,3 +6,30 @@ let board = [
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0]
 ]
+
+console.log(board[0][0])
+
+let currentPlayer = 1
+let gameOver = false
+let winner = null
+
+const btnRestartEl = document.getElementById("restart")
+
+function restartGame(){
+     board = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
+    
+     ]
+    currentPlayer = 1
+    gameOver = false
+    winner = null
+
+    console.log(board[0][0])
+}
+
+
